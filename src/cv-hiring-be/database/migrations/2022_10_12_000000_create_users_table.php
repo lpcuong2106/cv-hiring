18f4d801fc739eb34683d64394b49b1580cbca11
lpcuong2106 <lpcuong2106@gmail.com>
2022-01-09 10:38:40 +0700

    init table user

---
+++ src/cv-hiring-be/database/migrations/2022_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->timestamp('birthday');
+            $table->tinyInteger('gender');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('address');
+            $table->string('phone');
+            $table->bigInteger('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->string('province_id');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
