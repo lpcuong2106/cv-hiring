58c0b547711d4265154bb608ccfd265b2317ce34
lpcuong2106 <lpcuong2106@gmail.com>
2022-05-02 22:48:03 +0700

    update config

--- src/cv-hiring-fe/next.config.js
+++ src/cv-hiring-fe/next.config.js
@@ -4,5 +4,10 @@ module.exports = {
   images: {
     domains: [],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   swcMinify: false,
 };
